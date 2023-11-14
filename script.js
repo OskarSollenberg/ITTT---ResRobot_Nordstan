6d26373547960d7f1bd2e24f791029054470a265
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-11-14 09:35:56 +0100

    Added files, with collected live times from trafiklab

---
+++ script.js
@@ -0,0 +1,46 @@
+////
+////
+////
+////
+"use strict";
+
+function buildList(departures) {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(departures));
+    ul.appendChild(li);
+}
+
+let url =
+    "https://api.resrobot.se/v2.1/departureBoard?id=740000002&format=json&accessId=d8a53cbb-324f-4aa5-be0a-63c39f3895b6";
+
+async function getData() {
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data);
+
+    let stop = [];
+    let name = [];
+    let times = [];
+
+    data.Departure.forEach((depature) => {
+        if (depature.stop.includes("Nordstan")) {
+            ////
+            ////
+            times.push(depature.time);
+            stop.push(depature.stop);
+            name.push(depature.name);
+
+            // buildList(depature.stop);
+            // buildList(depature.name);
+            // buildList(depature.time);
+        }
+    });
+
+    buildList(stop[0]);
+    buildList(times[0]);
+    buildList(name[0]);
+
+    console.log(times);
+}
+getData();
