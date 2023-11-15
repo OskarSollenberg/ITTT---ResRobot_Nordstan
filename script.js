0049d10f618c6814d49c4cd0c68c2e5349a00eb1
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-11-15 09:31:20 +0100

    nothing really

--- script.js
+++ script.js
@@ -14,54 +14,74 @@ let url =
 //     }
 // }
 
+const busTime = document.querySelector("#time");
+const busStop = document.querySelector("#stop");
+const busName = document.querySelector("#name");
+
+let busTimes = [];
+let busStops = [];
+let busNames = [];
+let busDirections = [];
+
 async function fetchAndDisplayBus() {
     let res = await fetch(url);
     let data = await res.json();
 
     data.Departure.forEach((departure) => {
-        if (departure.stop.includes("Nordstan")) {
-            ////
-            const busTime = document.querySelector("#time");
-            const busStop = document.querySelector("#stop");
-            const busName = document.querySelector("#name");
-
-            let busTimes = [];
-            let busStops = [];
-            let busNames = [];
-
+        if (
+            departure.stop.includes("Nordstan") &&
+            departure.name.includes("16")
+        ) {
             busTimes.push(departure.time);
-            busStops.push(departure.stop);
-            busNames.push(departure.name);
+            // console.log(busTimes)
+            // busStops.push(departure.stop);
+            // busNames.push(departure.name);
+            busDirections.push(departure.direction);
 
-            console.log(busTimes);
+            // console.log(departure);
 
-            busTime.textContent = busTimes[0];
-            busStop.textContent = busStops[0];
-            busName.textContent = busNames[0];
-
-            const timeArr = departure.time.split(":");
-            const h = timeArr[0];
-            const m = timeArr[1];
-            const s = timeArr[2];
-
-            const busTimeMs = h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000;
-
-            const now = new Date();
-            const midnight = new Date(
-                now.getFullYear(),
-                now.getMonth(),
-                now.getDate()
-            );
-            const currentTimeMs = now - midnight;
-
-            const busLeavesInMs = busTimeMs - currentTimeMs;
-
-            setTimeout(function () {
-                fetchAndDisplayBus();
-            }, busLeavesInMs);
+            if (busDirections[0].includes("Bockkranen")) {
+                busTime.textContent = `Bockkranen ${busTimes[0]}`;
+                ////
+            } else if (busDirections[0].includes("Fyrktorget")) {
+                busTime.textContent = `Fyrktorget ${busTimes[0]}`;
+            }
         }
+
+        // busName.textContent = `${busNames[0]}`;
+        // busStop.textContent = `${busStops[0]} ${busStops[1]} ${busStops[2]} ${busStops[3]} ${busStops[4]}`;
+
+        // busStop.textContent = busStops[0];
+        // busName.textContent = busNames[0];
     });
+    delay();
 }
 fetchAndDisplayBus();
 
 // setInterval(getData, 1000);
+
+function delay() {
+    const timeArr = busTimes[0].split(":");
+    const h = timeArr[0];
+    const m = timeArr[1];
+    const s = timeArr[2];
+
+    const busTimeMs = h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000;
+
+    const now = new Date();
+    const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const currentTimeMs = now - midnight;
+
+    const busLeavesInMs = busTimeMs - currentTimeMs;
+
+    console.log(busTimes[0]);
+    console.log(busLeavesInMs);
+
+    setTimeout(function () {
+        fetchAndDisplayBus();
+    }, busLeavesInMs - 5000);
+}
+
+// console.log(busTimes);
+// console.log(busStops);
+// console.log(busNames);
